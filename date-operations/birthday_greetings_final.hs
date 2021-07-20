64e4ec929a3e7ea9b29c971ecfb9afc20d1bc496
Zhiyang Ong <zhiyang.ong@tamu.edu>
2021-07-20 07:28:00 -0500

    Fix indentation

--- date-operations/birthday_greetings_final.hs
+++ date-operations/birthday_greetings_final.hs
@@ -1,10 +1,10 @@
 import Data.Time
 -- Function to get the current date.
 findCurrentTime = do
-	  zt <- getZonedTime
-	  let s1 = show zt
-	  let s2 = drop 5 $ take 10 s1
-	  return s2
+	zt <- getZonedTime
+	let s1 = show zt
+	let s2 = drop 5 $ take 10 s1
+	return s2
 -- Function to store the birthday as a string and return it.
 getBirthday = do
 	  birthday <- "07-20"
